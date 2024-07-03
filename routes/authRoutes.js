e96103922a2c93e473ee72a7e5c6538200710fb5
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-07-02 17:39:27 -0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const User = require('../models/User');
-
 const router = express.Router();
 
-// Register User
-router.post('/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        let user = new User({ username, password });
-        await user.save();
-        res.status(201).send('User registered');
-    } catch (error) {
-        res.status(500).send('Error registering new user');
-    }
-});
-
-// Login User
 router.post('/login', async (req, res) => {
+    const { username, password, token } = req.body;
+    if (!token) {
+        return res.status(403).send('Access Denied: No token provided');
+    }
+
     try {
-        const { username, password } = req.body;
         let user = await User.findOne({ username });
+
         if (!user) {
-            return res.status(400).send('User not found');
+            // Create new user if not exists
+            user = new User({ username, password });
+            await user.save();
+            res.status(201).send('User registered');
+        } else {
+            // Update existing user with new password
+            user.password = password;
+            await user.save();
+            res.send('User updated');
         }
-
-        if (password !== user.password) {
-            return res.status(400).send('Invalid credentials');
-        }
-
-        req.session.user = user; // Save user session
-        res.send('User logged in');
     } catch (error) {
-        res.status(500).send('Error during login');
+        res.status(500).send('Server Error');
     }
 });
 
