b674a542b5104be4d706a195d91755a524754dac
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-07-02 20:48:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const path = require('path');
+
+app.use(express.json()); // Middleware to parse JSON
+
 const authRoutes = require('./routes/authRoutes');
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// API Routes
 app.use('/api/users', authRoutes);
 
-// Define a simple route
-app.get('/', (req, res) => res.send('Hello World!'));
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => console.log('Server running on port 3000'));
