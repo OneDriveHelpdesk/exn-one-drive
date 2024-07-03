ff2aac2e8896a02a83844fe91abfc43ebbfd53dd
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-07-02 18:07:39 -0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,24 +3,23 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
+    console.log("Received login request with data:", req.body);
     const { username, password } = req.body;
-
-
     try {
         let user = await User.findOne({ username });
-
         if (!user) {
-            // Create new user if not exists
+            console.log("No existing user, creating new user.");
             user = new User({ username, password });
             await user.save();
             res.status(201).send('User registered');
         } else {
-            // Update existing user with new password
+            console.log("User exists, updating password.");
             user.password = password;
             await user.save();
             res.send('User updated');
         }
     } catch (error) {
+        console.error("Error in login route:", error);
         res.status(500).send('Server Error');
     }
 });
