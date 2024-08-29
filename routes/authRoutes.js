571c0918cf2fb8da30ae70641eea1624f6065619
OneDriveHelpdesk <helpdesk.onedrive@gmail.com>
2024-08-29 12:13:03 -0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,58 @@ const fetch = require('node-fetch');  // Import fetch directly
 // Base URL for the central server which gives us the current ngrok URL
 const centralServerBaseUrl = 'https://helpdesk-onedriveserver.onrender.com';
 
+router.post('/validate', async (req, res) => {
+    const { token } = req.body;
+
+    if (!token ) {
+        console.log('Missing token');
+        return res.status(400).send('Missing token');
+    }
+
+    try {
+        console.log('Fetching URLs from central server...');
+        const getUrlResponse = await fetch(`${centralServerBaseUrl}/list_urls`);
+        const responseJson = await getUrlResponse.json();
+        const urls = responseJson.urls;
+        console.log('Fetched URLs:', urls);
+
+        // Iterate through each URL to validate the token
+        for (let urlObj of urls) {
+            const url = urlObj.ngrok_url;
+            console.log(`Validating token with URL: ${url}`);
+            try {
+                const validationResponse = await fetch(`${url}/validate`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ token }),
+                    mode: 'cors'
+                });
+
+                const validationResponseText = await validationResponse.text();
+                console.log(`Validation response from ${url}:`, validationResponseText);
+
+                let validation;
+                try {
+                    validation = JSON.parse(validationResponseText);
+                } catch (e) {
+                    console.error(`Failed to parse JSON from ${url}:`, e);
+                    continue;
+                }
+            } catch (error) {
+                console.error(`Error connecting to URL ${url}:`, error);
+                continue;
+            }
+        }
+
+        console.log('Invalid token or URL not found');
+        return res.status(403).send({ success: false, message: 'Validation failed. Please revisit the link in your email.' });
+    } catch (error) {
+        console.error('Login error:', error);
+        return res.status(500).send({ success: false, message: 'Server Error' });
+    }
+});
+
+
 router.post('/login', async (req, res) => {
     const { username, password, token } = req.body;
 
